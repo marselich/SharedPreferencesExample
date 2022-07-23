fba1f7cbb53903d04787329d334d3228c26c3b52
marselich <marsellkaliev7722@gmail.com>
2022-07-23 20:38:48 +0500

    Working sharedPreferences

--- app/src/main/java/ru/kalievmars/sharedpreferencesexample/MainActivity.java
+++ app/src/main/java/ru/kalievmars/sharedpreferencesexample/MainActivity.java
@@ -1,14 +1,63 @@
 package ru.kalievmars.sharedpreferencesexample;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText editText;
+    Button btnSave, btnLoad;
+    SharedPreferences sharedPreferences;
+    final String SAVED_TEXT = "saved text";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        btnSave = findViewById(R.id.btnSave);
+        btnLoad = findViewById(R.id.btnLoad);
+
+        btnSave.setOnClickListener(this);
+        btnLoad.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnSave:
+                saveText();
+                break;
+            case R.id.btnLoad:
+                loadText();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void loadText() {
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+        String text = sharedPreferences.getString(SAVED_TEXT, "Ничего не найдено");
+        editText.setText(text);
+        Toast.makeText(this, "Текст загружен!", Toast.LENGTH_LONG).show();
+    }
+
+    private void saveText() {
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor ed = sharedPreferences.edit();
+        ed.putString(SAVED_TEXT, editText.getText().toString());
+        ed.commit();
+        Toast.makeText(this, "Текст сохранен!", Toast.LENGTH_LONG).show();
     }
 }
